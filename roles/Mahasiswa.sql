0dfa9e2184c577b765f19311644322a1aa4d37a0
shikajiko <riyanfadli6675@gmail.com>
2026-06-13 21:13:35 +0700

    rename policy on mahasiswa

--- roles/Mahasiswa.sql
+++ roles/Mahasiswa.sql
@@ -7,7 +7,7 @@ GRANT SELECT, INSERT ON Peminjaman_Ruangan TO mahasiswa_role;
 
 ALTER TABLE Peminjaman ENABLE ROW LEVEL SECURITY;
 
-CREATE POLICY mahasiswa_own_loans ON Peminjaman
+CREATE POLICY mahasiswa_own_peminjaman ON Peminjaman
     FOR ALL
     TO mahasiswa_role
     USING (Mahasiswa_nrp = current_setting('app.current_nrp'));
